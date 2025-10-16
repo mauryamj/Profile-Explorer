6332bd7556b7a5077aba12e6f79c6a80c5de05d8
mauryamj <mauryamj32@gmail.com>
2025-10-16 12:59:48 +0530

    created the project

---
+++ lib/features/profile_explorer/model/user_profile.dart
@@ -0,0 +1,7 @@
+class UserProfile {
+  final String name;
+  final String profileimage;
+  final int age;
+  final String city;
+  UserProfile(this.name, this.profileimage, this.age, this.city);
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:profile_explorer/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return App();
+  }
+}
